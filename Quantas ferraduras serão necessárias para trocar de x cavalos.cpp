65c56b09cde26ed74101264ed4913c91239d913e
Nicolas-1802 <101649630+Nicolas-1802@users.noreply.github.com>
2023-09-21 19:53:04 -0300

    Create Quantas ferraduras serão necessárias para trocar de x cavalos.cpp

---
+++ Quantas ferraduras serão necessárias para trocar de x cavalos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int cavalo,ferr;
+
+    cout<<"Insira quantos cavalos: "<<endl;
+    cin>>cavalo;
+
+    ferr=cavalo*4;
+
+    cout<<"Sera necessario"<<" "<<ferr<<" "<<"ferraduras"<<endl;
+
+
+
+    return 0;
+}
