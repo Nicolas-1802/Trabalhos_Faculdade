6f728bb3475758683b79c44144ed513c96c2edd4
Nicolas-1802 <101649630+Nicolas-1802@users.noreply.github.com>
2023-09-21 19:39:34 -0300

    Add files via upload

---
+++ Exercicio 1 Word.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nome;
+
+    cout<<"Insira o seu nome: "<<endl;
+    cin>>nome;
+    cout<<"Ola "<<nome<<endl;
+    return 0;
+}
