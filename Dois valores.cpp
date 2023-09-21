e52ee48897de0905f5a7f33f630f0cad70e09606
Nicolas-1802 <101649630+Nicolas-1802@users.noreply.github.com>
2023-09-21 19:56:16 -0300

    Create Dois valores.cpp
    Inserir dois valores e mostrar a soma, subtração, multiplicação e divisão deles.

---
+++ Dois valores.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+
+    cout << "Insira dois valores: "<<endl;
+    cin>>n1>>n2;
+
+    cout<<"Soma: "<<n1+n2<<endl
+    <<"Subtracao: "<<n1-n2<<endl
+    <<"Multiplicacao: "<<n1*n2<<endl
+    <<"Divisao: "<< n1/n2<<endl;
+
+    return 0;
+}
