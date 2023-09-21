ddf1c66bcfb306992818311ad02083a8c074f5c5
Nicolas-1802 <101649630+Nicolas-1802@users.noreply.github.com>
2023-09-21 19:42:38 -0300

    Add files via upload

---
+++ Exercicio 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, dobro;
+
+    cout << "Insira um numero: " << endl;
+    cin >> n1;
+
+    dobro = n1 * 2;
+
+    cout << "O dobro desse numero eh: " << dobro << endl;
+
+
+
+    return 0;
+}
